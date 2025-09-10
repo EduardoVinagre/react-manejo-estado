5e7b99c190b56db7617b7ce338753d3895cc0053
Eduardo Vinagre <eduardovinagre@gmail.com>
2025-09-10 16:12:32 -0600

    Add loading control to component using an exteded class from React.Component

--- src/components/ClassState.js
+++ src/components/ClassState.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             error: true,
-            success: false
+            loading: false
         };
+    }
+
+    componentDidUpdate(){
+        console.log('Inicio');
+        if(!!this.state.loading){
+            setTimeout(()=> {
+                console.log('Iniciando la validacion');
+                console.log('Terminando la validacion')
+                this.setState({loading: false});
+            }, 3000);    
+        }
+        console.log('fin');
     }
 
     render() {
@@ -16,8 +29,13 @@ class ClassState extends React.Component {
                 <p>Por favor, escribe el código de seguridad para confirmar que quieres eliminar</p>
                 {this.state.error && 
                     (<p>Error: El código es incorrecto</p>)}
+                {
+                    this.state.loading && (
+                        <Loading />
+                    )
+                }
                 <input placeholder="Código de seguridad"/>
-                <button onClick={() => this.setState({ error: !this.state.error })}>Comprobar</button>
+                <button onClick={() => this.setState({ loading: true })}>Comprobar</button>
             </div>
         )
     }
