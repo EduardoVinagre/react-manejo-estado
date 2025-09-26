e1befceca33c7dbc0e84ef88f6b728e63349bcf1
Eduardo Vinagre <eduardovinagre@gmail.com>
2025-09-26 01:38:35 -0600

    refactor: Add functions to change the state with some semantic meaning. onConfirm, onWrite, onReset, onCheck, etc

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -10,106 +10,128 @@ function UseState({ name }) {
         confirmed: false,
     });
 
-    React.useEffect(()=>{
-        console.log('Inicio');        
-        if(!!state.loading){
-            setTimeout(()=> {
+    const onConfirm = () => {
+        setState({
+            ...state,
+            loading: false,
+            error: false,
+            confirmed: true
+        })
+    }
+
+    const onError = () => {
+        setState({
+            ...state,
+            loading: false,
+            error: true
+        })
+    }
+
+    const onWrite = (newValue) => {
+        setState({
+            ...state,
+            value: newValue
+        });
+    }
+
+    const onCheck = () => {
+        setState({
+            ...state,
+            loading: true
+        })
+    }
+
+    const onDelete = () => {
+        setState({
+            ...state,
+            deleted: true
+        })
+    }
+
+    const onReset = () => {
+        setState({
+            ...state,
+            confirmed: false,
+            deleted: false,
+            value: ''
+        })
+    }
+
+    React.useEffect(() => {
+        console.log('Inicio');
+        if (!!state.loading) {
+            setTimeout(() => {
                 console.log('Iniciando la validacion');
                 setState({
                     ...state,
-                    loading:false,
+                    loading: false,
                 });
                 console.log(state);
-                if(state.value === SECURITY_CODE){
-                    setState({
-                        ...state,
-                        loading: false,
-                        error:false,
-                        confirmed: true
-                    })
+                if (state.value === SECURITY_CODE) {
+                    onConfirm();
                 }
-                else{
-                    setState({
-                        ...state,
-                        loading: false,
-                        error:true
-                    })
+                else {
+                    onError();
                 }
                 console.log(state);
                 console.log('Terminando la validacion');
-            }, 3000);    
+            }, 3000);
         }
         console.log('fin');
-    },[state.loading]);
+    }, [state.loading]);
 
-    if(!state.deleted && !state.confirmed) {
+    if (!state.deleted && !state.confirmed) {
         return (
             <div>
-                <h2>Eliminar { name }</h2>
+                <h2>Eliminar {name}</h2>
                 <p>Por favor, escribe el código de seguridad para confirmar que quieres eliminar</p>
 
-                {(state.error && !state.loading) && 
+                {(state.error && !state.loading) &&
                     (<p>Error: El código es incorrecto</p>)}
-                {state.loading && 
+                {state.loading &&
                     (<p>Cargando...</p>)}
-                <input 
-                placeholder="Código de seguridad"
-                value={state.value}
-                onChange={(event)=>{
-                    console.log(event.target.value);
-                    setState({
-                        ...state,
-                        value: event.target.value
-                    });
-                }}/>
-                <button onClick={()=> {setState({
-                    ...state,
-                    loading:true
-                    })}}>Comprobar</button>
+                <input
+                    placeholder="Código de seguridad"
+                    value={state.value}
+                    onChange={(event) => {
+                        console.log(event.target.value);
+                        onWrite(event.target.value);
+                    }} />
+                <button onClick={() => {
+                    onCheck();
+                }}>Comprobar</button>
             </div>
-            )
-    } else if (!!state.confirmed && !state.deleted){
+        )
+    } else if (!!state.confirmed && !state.deleted) {
         return (
             <React.Fragment>
                 <p>Esta seguro de eliminar</p>
-                <button 
+                <button
                     onClick={
-                        ()=>{
-                            setState({
-                                ...state,
-                                deleted: true
-                            })
+                        () => {
+                            onDelete();
                         }
                     }>Si, eliminar</button>
                 <button onClick={
-                        ()=>{
-                            setState({
-                                ...state,
-                                confirmed: false,
-                                value: ''
-                            })
-                        }
-                    }>No, conservar</button>
+                    () => {
+                        onReset()
+                    }
+                }>No, conservar</button>
             </React.Fragment>
         )
     } else {
         return (
             <React.Fragment>
                 <p>Eliminado con éxito</p>
-                                <button onClick={
-                        ()=>{
-                            setState({
-                                ...state,
-                                confirmed: false,
-                                deleted: false,
-                                value: ''
-                            })
-                        }
-                    }>Recuperar estado</button>
+                <button onClick={
+                    () => {
+                        onReset();
+                    }
+                }>Recuperar estado</button>
             </React.Fragment>
         )
     }
-    
+
 }
 
 export { UseState };
